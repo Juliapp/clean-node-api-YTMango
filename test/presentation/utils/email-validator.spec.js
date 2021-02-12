5ae5d97f64487fbce77565f57268ea85dd3063e2
Juli <aragaopintojuli@gmail.com>
2021-02-12 10:41:18 -0300

    ensure email validator to return current email

--- test/presentation/utils/email-validator.spec.js
+++ test/presentation/utils/email-validator.spec.js
@@ -5,17 +5,28 @@ class EmailValidator {
   }
 }
 
+const makeSut = () => {
+  const sut = new EmailValidator()
+  return { sut }
+}
+
 describe('Email Validator', () => {
   test('Should return true id validator returns true', () => {
-    const sut = new EmailValidator()
+    const { sut } = makeSut()
     const isEmailValid = sut.isValid('valid_email@email.com')
     expect(isEmailValid).toBe(true)
   })
 
   test('Should return false id validator returns false', () => {
-    const sut = new EmailValidator()
     validator.isEmailValid = false
+    const { sut } = makeSut()
     const isEmailValid = sut.isValid('invalid_email@email.com')
     expect(isEmailValid).toBe(false)
+  })
+
+  test('Should call validator with correct email', () => {
+    const { sut } = makeSut()
+    sut.isValid('any_email@email.com')
+    expect(validator.email).toBe('any_email@email.com')
   })
 })
