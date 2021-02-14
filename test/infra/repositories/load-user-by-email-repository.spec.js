6c35a8d5e0df1bfac245f1cfe9ea2b6455ac8254
Juli <aragaopintojuli@gmail.com>
2021-02-14 17:59:22 -0300

    ensure LoadUserByEmailRepository return null if no user is found

---
+++ test/infra/repositories/load-user-by-email-repository.spec.js
@@ -0,0 +1,20 @@
+class LoadUserByEmailRepository {
+  async load (email) {
+    return null
+  }
+}
+
+const makeSut = () => {
+  const sut = new LoadUserByEmailRepository()
+  return {
+    sut
+  }
+}
+
+describe('LoadUserByEmailRepository', () => {
+  test('Should return null if no user is found', async () => {
+    const { sut } = makeSut()
+    const user = await sut.load('invalid_email@mail.com')
+    expect(user).toBeNull()
+  })
+})
