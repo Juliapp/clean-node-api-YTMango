16d40b625d441fcf8a2aaf4f5e61053644a3c0fc
Juli <aragaopintojuli@gmail.com>
2021-02-19 21:13:20 -0300

    enable cors

--- test/main/config/setup.test.js
+++ test/main/config/setup.test.js
@@ -9,4 +9,15 @@ describe('App Setup', () => {
     const response = await request(app).get('/test_x-powered_by')
     expect(response.headers['x-powered-by']).toBeUndefined()
   })
+
+  test('Should enable CORS', async () => {
+    app.get('/test_cors', (req, res) => {
+      res.send('')
+    })
+
+    const response = await request(app).get('/test_cors')
+    expect(response.headers['access-control-allow-origin']).toBe('*')
+    expect(response.headers['access-control-allow-methods']).toBe('*')
+    expect(response.headers['access-control-allow-headers']).toBe('*')
+  })
 })
