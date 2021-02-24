c02086c5826702bdc37e0d8d5768d178537ac0e5
Juliapp <aragaopintojuli@gmail.com>
2021-02-24 09:06:37 -0300

    changing email validator path

--- test/utils/email-validator.spec.js
+++ test/utils/email-validator.spec.js
@@ -1,4 +1,4 @@
-const EmailValidator = require('../../src/utils/email-validator')
+const EmailValidator = require('../../src/utils/helpers/email-validator')
 const validator = require('validator')
 
 const makeSut = () => {
